8156996d2d0eb5bef0d2c4e4db65247161fceb9e
Администратор <@10.81.2.197>
2012-07-17 20:48:00 +0400

    Revert "Добавил 2"
    This reverts commit e7a7ce5c5c1ed8617f8d3a04d1d3646ab112d4f5.

--- Test/src/Test/java.java
+++ Test/src/Test/java.java
@@ -13,7 +13,7 @@ public class java {
     /**
      * @param args the command line arguments
      */
-    public static void main(float[] args) {
+    public static void main(String[] args) {
         // TODO code application logic here
         
         
